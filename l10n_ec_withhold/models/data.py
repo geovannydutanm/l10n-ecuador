4be47607f36e3d545bb42e6f2e60620a9b7d91dc
Geovany Dutan <nelson.dutan@altia.es>
2026-02-28 20:35:13 +0100

    [I18N] l10n_ec_withhold: fix es_EC tax support labels

--- l10n_ec_withhold/models/data.py
+++ l10n_ec_withhold/models/data.py
@@ -2,20 +2,20 @@ TAX_SUPPORT = [
     ("00", "00 - Not applicable"),
     (
         "01",
-        "01 - Tax credit for VAT return \
-         (services and goods other than inventories and fixed assets)",
+        "01 - Tax credit for VAT return "
+        "(services and goods other than inventories and fixed assets)",
     ),
     (
         "02",
-        "02 - Cost or expense for income tax return \
-        (services and goods other than inventories and fixed assets)",
+        "02 - Cost or expense for income tax return "
+        "(services and goods other than inventories and fixed assets)",
     ),
     ("03", "03 - Fixed asset - Tax credit for VAT return"),
     ("04", "04 - Fixed asset - Cost or expense for income tax return"),
     (
         "05",
-        "05 - Travel, lodging and meals settlement for income tax \
-        (in employees' names and not in the company's name)",
+        "05 - Travel, lodging and meals settlement for income tax "
+        "(in employees' names and not in the company's name)",
     ),
     ("06", "06 - Inventory - Tax credit for VAT return"),
     ("07", "07 - Inventory - Cost or expense for income tax return"),
@@ -26,7 +26,6 @@ TAX_SUPPORT = [
     ("12", "12 - Presumptive taxes and withholdings"),
     (
         "13",
-        "13 - Amounts recognized by public sector entities \
-        in favor of taxpayers",
+        "13 - Amounts recognized by public sector entities " "in favor of taxpayers",
     ),
 ]
